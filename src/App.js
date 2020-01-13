4b8ae61df1e7f9a03afe602d9eed433c569ea431
Erick Trettel de Almeida <ericktrettel@gmail.com>
2020-01-13 11:31:04 -0300

    [new] Creating a fake server with json-server [new] Creating Todo component [new] Adding react-app-rewired to project [new] Adding workbox-webpack-plugin to project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
 import Todo from './Todo'
 
-const Todo = () => {
-  return <p>todo</p>
-}
-
 class App extends Component {
   render() {
     return <Todo />
